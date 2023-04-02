e905ce75897debbe1b70f49d1bccd091a267cf94
nima ghanbari <nimagithub1401@gmail.com>
2023-04-03 03:17:16 +0430

    add admin friendship

--- Friendship/admin.py
+++ Friendship/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Friendship
 
-# Register your models here.
+@admin.register(Friendship)
+class FriendshipAdmin(admin.ModelAdmin):
+    list_display = ['id','request_from','request_to','is_accepted']
+    
+    def has_add_permission(self, request):
+        return False
+    def has_change_permission(self, request):
+        return False
+    def has_delete_permission(self, request):
+        return False
+    
